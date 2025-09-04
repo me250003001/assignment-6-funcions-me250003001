9f75cef5e1d1b1f4930342f5bda1aa7cbd00ab2b
me250003001 <me250003001@iiti.ac.in>
2025-09-04 11:24:45 +0530

    Update gcd.cpp

--- gcd.cpp
+++ gcd.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 using namespace std;
 
-// Recursive function to find GCD of two numbers
+/* Recursive function to find GCD of two numbers */
 int gcd(int a, int b) {
     // TODO: complete the function as per instructions
-    
+  if (b==0){
+  return a;  
 }
-
+  return gcd(b, a%b);
+  }
 int main() {
     int num1, num2;
     cin >> num1 >> num2;
@@ -15,4 +17,3 @@ int main() {
 
     return 0;
 }
-
